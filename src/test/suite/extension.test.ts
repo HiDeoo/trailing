bf2d19d92934ceae75abcf5ea687c71c54298e4b
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2022-07-23 11:16:28 +0200

    test: add more toggling tests

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,29 +1,76 @@
-import { commands } from 'vscode'
+import { commands, Position, Selection } from 'vscode'
 
 import { type TrailingCommand, TrailingDefinitions, type TrailingSymbol } from '../../extension'
 import { assertDocumentTextEqual, withEditor } from '../utils'
 
 function runTestsWithCommandAndSymbol(command: TrailingCommand, symbol: TrailingSymbol) {
   suite(`Trailing Symbol '${symbol}'`, () => {
-    test(`should add '${symbol}' at the end`, () =>
+    test(`should add trailing '${symbol}'`, () =>
       withEditor('test', async (document) => {
         await commands.executeCommand(command)
 
         assertDocumentTextEqual(document, `test${symbol}`)
       }))
 
-    test(`should remove '${symbol}' at the end`, () =>
+    test(`should remove trailing '${symbol}'`, () =>
       withEditor(`test${symbol}`, async (document) => {
         await commands.executeCommand(command)
 
         assertDocumentTextEqual(document, 'test')
       }))
 
-    test(`should toggle '${symbol}' at the end`, () =>
+    test(`should toggle trailing '${symbol}'`, () =>
       withEditor('test', async (document) => {
         await commands.executeCommand(command)
 
         assertDocumentTextEqual(document, `test${symbol}`)
+
+        await commands.executeCommand(command)
+
+        assertDocumentTextEqual(document, 'test')
+      }))
+
+    test(`should toggle trailing '${symbol}' with the cursor at the end of the line`, () =>
+      withEditor('test', async (document, editor) => {
+        let position = new Position(0, 4)
+        editor.selection = new Selection(position, position)
+
+        await commands.executeCommand(command)
+
+        assertDocumentTextEqual(document, `test${symbol}`)
+
+        position = new Position(0, 5)
+        editor.selection = new Selection(position, position)
+
+        await commands.executeCommand(command)
+
+        assertDocumentTextEqual(document, 'test')
+      }))
+
+    test(`should toggle trailing '${symbol}' with a line partially selected`, () =>
+      withEditor('test', async (document, editor) => {
+        editor.selection = new Selection(new Position(0, 0), new Position(0, 2))
+
+        await commands.executeCommand(command)
+
+        assertDocumentTextEqual(document, `test${symbol}`)
+
+        editor.selection = new Selection(new Position(0, 3), new Position(0, 5))
+
+        await commands.executeCommand(command)
+
+        assertDocumentTextEqual(document, 'test')
+      }))
+
+    test(`should toggle trailing '${symbol}' with a line entirely selected`, () =>
+      withEditor('test', async (document, editor) => {
+        editor.selection = new Selection(new Position(0, 0), new Position(0, 4))
+
+        await commands.executeCommand(command)
+
+        assertDocumentTextEqual(document, `test${symbol}`)
+
+        editor.selection = new Selection(new Position(0, 0), new Position(0, 5))
 
         await commands.executeCommand(command)
 
